a3519dca621de3c41904c6702e6c9adbb89137d1
Ivan Puha <unsaightly@gmail.com>
2025-06-14 17:02:02 -0400

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>StarWars app</div>
+    </>
+  )
+}
+
+export default App
