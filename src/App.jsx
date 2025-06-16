35dd85bf9ecac4132cc6a2f7cd822883eacc61a1
Ivan Puha <unsaightly@gmail.com>
2025-06-15 22:41:17 -0400

    added input filter

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState("people");
   const [hasMore, setHasMore] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const observer = useRef();
 
   const fetchData = async (pageNumber, type) => {
@@ -50,23 +51,33 @@ function App() {
     if (node) observer.current.observe(node);
   }, [isLoading, hasMore]);
 
+  const filteredData = data.filter(card =>
+    card.name?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="flex flex-col justify-center items-center m-3 p-3">
-      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+    <div className="flex flex-col justify-center items-center m-6 px-4 space-y-6">
+      <Header
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
 
-      {data.map((item, index) => {
-        const isLast = index === data.length - 1;
-        return (
-          <PersonCard
-            key={item.url}
-            card={item}
-            ref={isLast ? lastRef : null}
-            currentPage={currentPage}
-          />
-        );
-      })}
-
-      {isLoading && page > 1 && <p>Loading more...</p>}
+      <div className="w-full flex flex-col items-center space-y-4">
+        {filteredData.map((item, index) => {
+          const isLast = index === data.length - 1;
+          return (
+            <PersonCard
+              key={item.url}
+              card={item}
+              ref={isLast ? lastRef : null}
+              currentPage={currentPage}
+            />
+          );
+        })}
+        {isLoading && page > 1 && <p className="text-yellow-200">Loading more...</p>}
+      </div>
     </div>
   );
 }
